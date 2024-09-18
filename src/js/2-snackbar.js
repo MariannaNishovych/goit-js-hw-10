cdeb8cd8a681d774cc11cdb95065cd054c8c4f39
Marianna Nishovych <nisovicmarianna@gmail.com>
2024-09-18 09:19:12 +0300

    add correct snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -7,49 +7,47 @@ const form = document.querySelector('.form');
 
 form.addEventListener('submit', (event) => {  
     event.preventDefault();  
-    
+
     const delay = Number(event.target.elements.delay.value);  
     const isResolved = event.target.elements.state.value === 'fulfilled';  
 
     const promise = new Promise((resolve, reject) => {  
-        if (isResolved) {  
-            resolve(delay);  
-        } else {  
-            reject(delay);  
-        }  
+        setTimeout(() => {  
+            if (isResolved) {  
+                resolve(`Fulfilled promise in ${delay}ms`);  
+            } else {  
+                reject(`Rejected promise in ${delay}ms`);  
+            }  
+        }, delay);  
     });  
 
     promise  
-        .then(() => {  
-            setTimeout(() => {  
-                iziToast.success({  
-                    title: 'Ok',  
-                    message: `Fulfilled promise in ${delay}ms`,  
-                    position: 'topRight',  
-                    color: '#59a10d',  
-                    titleColor: '#fff',  
-                    titleSize: '16px',  
-                    titleLineHeight: '150%',  
-                    messageColor: '#fff',  
-                    messageSize: '16px',  
-                    messageLineHeight: '150%',  
-                });  
-            }, delay);  
+        .then((message) => {  
+            iziToast.success({  
+                title: 'Ok',  
+                message,  
+                position: 'topRight',  
+                color: '#59a10d',  
+                titleColor: '#fff',  
+                titleSize: '16px',  
+                titleLineHeight: '150%',  
+                messageColor: '#fff',  
+                messageSize: '16px',  
+                messageLineHeight: '150%',  
+            });  
         })  
-        .catch(() => {  
-            setTimeout(() => {  
-                iziToast.error({  
-                    title: 'Error',  
-                    message: `Rejected promise in ${delay}ms`,  
-                    position: 'topRight',  
-                    color: '#ef4040',  
-                    titleColor: '#fff',  
-                    titleSize: '16px',  
-                    titleLineHeight: '150%',  
-                    messageColor: '#fff',  
-                    messageSize: '16px',  
-                    messageLineHeight: '150%',  
-                });  
-            }, delay);  
+        .catch((message) => {  
+            iziToast.error({  
+                title: 'Error',  
+                message,  
+                position: 'topRight',  
+                color: '#ef4040',  
+                titleColor: '#fff',  
+                titleSize: '16px',  
+                titleLineHeight: '150%',  
+                messageColor: '#fff',  
+                messageSize: '16px',  
+                messageLineHeight: '150%',  
+            });  
         });  
-});  
+}); 
